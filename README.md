7155f3fd0ad70e0c560dde4502dd779480a9008a
xiaoshenke <1205914137@qq.com>
2018-02-10 19:50:46 +0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SlightSniffer
+
+这个项目本来是准备用spark做文本数据挖掘的。弃坑了。
+目前里面就实现了一个DirectoryWatcher。
--- src/test/java/wuxian/me/slightsniffer/DirectoryInputDStreamTest.scala
+++ src/test/java/wuxian/me/slightsniffer/DirectoryInputDStreamTest.scala
@@ -4,26 +4,23 @@ import org.apache.spark.mllib.stat.test.{BinarySample, StreamingTest}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.util.Utils
 
-
-
 /**
  * Created by wuxian on 23/12/2017.
  */
 object DirectoryInputDStreamTest {
 
     def main(args: Array[String]) {
-
         val dataDir = "/Users/wuxian/Desktop"
         val batchDuration = Seconds(1)
-
 
         val conf = new SparkConf().setMaster("local").setAppName("StreamingTestExample")
         val ssc = new StreamingContext(conf, batchDuration)
         //ssc.checkpoint {val dir = Utils.createTempDir() dir.toString}
 
         var sc = new DirectoryStreamingContext(ssc)
-        sc.createDirectoryStream(dataDir).map(name => {println(name)
+        var rdd  = sc.createDirectoryStream(dataDir).map(name => {println("文件: "+name)
             name})
+        rdd.print()
 
         ssc.start()
         ssc.awaitTermination()
